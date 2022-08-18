142429a5873cd0d5ca2bbc5b45a20c985e819541
Rayzor Arboleya <rayzor@arboleya.me>
2022-08-18 17:19:11 -0300

    Door 50%

--- components/Porta.jsx
+++ components/Porta.jsx
@@ -6,8 +6,10 @@ export default function Porta(props){
             <div className={styles.estrutura}>
                 <div className={styles.porta}>
                     <div className={styles.numero}>3</div>
+                    <div className={styles.macaneta}></div>
                 </div>
             </div>
+            <div className={styles.boden}></div>
         </div>
     )
 
